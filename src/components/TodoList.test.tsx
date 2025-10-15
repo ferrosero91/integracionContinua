d71c27eb91e9bdefb9274a112de527a05bf03f5c
ferrosero91 <ferrosero91@gmail.com>
2025-10-14 20:34:29 -0500

    Prueba con componente TodoList, con error intencional para probar flujo github actions

--- src/components/TodoList.test.tsx
+++ src/components/TodoList.test.tsx
@@ -21,7 +21,7 @@ describe("TodoList Component", () => {
     render(<TodoList />);
 
     const input = screen.getByPlaceholderText("Nueva tarea");
-    const addButton = screen.getByText("Agregar");
+    const addButton = screen.getByText("Agregarr");
 
     // Agregar tarea
     fireEvent.change(input, { target: { value: "Estudiar React" } });
